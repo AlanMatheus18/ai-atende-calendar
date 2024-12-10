a54e114102f91f9967c9ed5bd8c78bbe5497f840
Douglas Augusto <sunshinedn2003@gmail.com>
2024-12-10 17:50:24 -0300

    texto mudado

--- src/components/CircularIndeterminate.jsx
+++ src/components/CircularIndeterminate.jsx
@@ -75,7 +75,7 @@ export default function CircularIndeterminate() {
           fontSize: '2rem', 
         }}
       >
-        Loading...
+        Carregando Datas...
       </Typography>
     </Box>
   );
