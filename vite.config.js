d88a04db4794d14fa7fe784127b3b9f000eedfa8
Douglas Augusto <sunshinedn2003@gmail.com>
2024-12-12 23:53:10 -0300

    server config

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,8 @@ import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite';
 export default defineConfig({
   plugins: [react()],
-  base: '/site/calendar',
+  server: {
+    base: '/site/calendar',
+  }
 })
 
