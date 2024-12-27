93290113d4802998e9c8d8d1a52983cc3ea75d52
alanmatheus <matheusalan334@gmail.com>
2024-12-27 00:27:55 -0300

    Continuacao

--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,45 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router";
-// import axios from "axios";
 import Body from "./components/Body";
 import Header from "./components/Header";
 import Btnsend from "./components/Btnsend";
 import CircularTest from "./components/CircularTest/CircularTest";
 import ModalError from "./components/ModalError";
-// import Wait from "./utils/Wait";
-import Selectors from "./components/selectors";
+import Selectors from "./components/Selectors";
+import { HmacSHA1 } from "crypto-js";
 
 function App() {
-    const { hash } = useParams(); // Captura o hash da URL
-  const [data, setData] = useState([{"calendar": ["Agenda 1", "Agenda 2", "Agenda 3"],
-		"period": ["Semana atual", "Próxima semana", "Próximo mês"],
-		"turno": ["Manhã", "Tarde","Noite"],
-  "date": "27/12/2024",
-		"avaiableOptions": [
-			"10:00:00",
-			"10:30:00",
-			"11:00:00",
-			"11:30:00",
-			"12:00:00",
-			"12:30:00",
-			"13:00:00",
-			"13:30:00",
-			"14:00:00",
-			"14:30:00",
-			"15:00:00",
-			"15:30:00",
-			"16:00:00",
-			"16:30:00",
-			"17:00:00",
-			"17:30:00",
-			"18:00:00",
-			"18:30:00",
-			"19:00:00",
-			"19:30:00"
-		]
-
-  }]);
+  const { hash } = useParams(); // Captura o hash da URL
+  const [data, setData] = useState([
+    {
+      calendar: ["Agenda 1", "Agenda 2", "Agenda 3"],
+      period: ["Semana atual", "Próxima semana", "Próximo mês"],
+      turno: ["Manhã", "Tarde", "Noite"],
+      date: "27/12/2024",
+      avaiableOptions: [
+        "10:00:00",
+        "10:30:00",
+        "11:00:00",
+        "11:30:00",
+        "12:00:00",
+        "12:30:00",
+        "13:00:00",
+        "13:30:00",
+        "14:00:00",
+        "14:30:00",
+        "15:00:00",
+        "15:30:00",
+        "16:00:00",
+        "16:30:00",
+        "17:00:00",
+        "17:30:00",
+        "18:00:00",
+        "18:30:00",
+        "19:00:00",
+        "19:30:00",
+      ],
+    },
+  ]);
   const [dataLoading, setDataLoading] = useState(true);
   const [loading, setLoading] = useState(false);
   const [currentIndex, setCurrentIndex] = useState(0);
@@ -48,47 +49,6 @@ function App() {
   const [selectedCalendar, setSelectedCalendar] = useState("");
   const [selectedPeriod, setSelectedPeriod] = useState("");
   const [selectedTurno, setSelectedTurno] = useState("");
-
-
-  // useEffect(() => {
-  //   const requestData = async () => {
-  //     if (!hash) {
-  //       // Se o hash não estiver presente na URL, exibe o modal de erro
-  //       setError("Hash não encontrado. Por favor, verifique o link.");
-  //       setOpenModal(true);
-  //       setLoading(false);
-  //       return;
-  //     }
-
-  //     try {
-  //       const obj = {
-  //         lead_id: hash,
-  //         account: {
-  //           id: 32000011,
-  //           subdomain: "kommoagendamento",
-  //           account_domain: "https://kommoagendamento.kommo.com",
-  //         },
-  //       };
-
-  //       const { data } = await axios.post(
-  //         "https://dentalsante.aiatende.dev.br/web/calendar/listEvents",
-  //         obj
-  //       );
-
-  //       await Wait(2000); // Simula um delay de 2 segundos
-  //       setDataLoading(true);
-  //       setData(data);
-  //       setLoading(false);
-  //     } catch (error) {
-  //       console.error("Erro ao buscar dados do calendário:", error);
-  //       setError("Erro ao buscar dados do calendário.");
-  //       setOpenModal(true);
-  //       setLoading(false);
-  //     }
-  //   };
-
-  //   requestData();
-  // }, [hash]);
 
   useEffect(() => {
     setSelectedTime(null);
@@ -144,27 +104,29 @@ function App() {
                 value={selectedTurno}
                 onChange={setSelectedTurno}
               />
+
               
+              <h1 style={{display:"flex"}}>
+                {data[currentIndex]?.date}
+              </h1>
+
               <Body
-              index={currentIndex}
-              setCurrentIndex={setCurrentIndex}
-              calendar={data}
-              setCalendar={setData}
-               times={data[currentIndex]?.avaiableOptions?.slice(0, 2)} // Mostra apenas os dois primeiros horários
-               selectedTime={selectedTime}
-               setSelectedTime={setSelectedTime}
+                index={currentIndex}
+                setCurrentIndex={setCurrentIndex}
+                calendar={data}
+                setCalendar={setData}
+                times={data[currentIndex]?.avaiableOptions?.slice(0, 2)} // Mostra apenas os dois primeiros horários
+                selectedTime={selectedTime}
+                setSelectedTime={setSelectedTime}
               />
               <Btnsend
                 selectedTime={selectedTime}
                 date={data[currentIndex]?.date}
               />
-              
             </>
-            
           )}
         </>
       )}
-      
     </>
   );
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,33 +1,44 @@
- 
 import * as React from 'react';
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
-// import Typography from '@mui/material/Typography';
-import Logo from '../assets/Artboard 2.png';
 import { Typography } from '@mui/material';
-
-
-
+import Logo from '../assets/Artboard 2.png';
 
 export default function Header({ index, setCurrentIndex, calendar }) {
   return (
     <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static" sx={{ boxShadow: '0 1px 25px 1px rgba(0, 0, 0, 0.2)', display: 'flex', flexDirection: 'row', alignItems: 'center', }}>
+      <AppBar 
+        position="static" 
+        sx={{ 
+          boxShadow: '0 1px 25px 1px rgba(0, 0, 0, 0.2)', 
+          display: 'flex', 
+          flexDirection: 'row', 
+          alignItems: 'center' 
+        }}
+      >
         <img src={Logo} alt="" style={{ height: '55px' }} />
-        <Box variant="h4" component="div" sx={{ flexGrow: 1, flexShrink: 1, display: 'flex', justifyContent: 'center' }}>
-
-
-
-          <Typography variant="h6" component="div" 
-          sx={{margin: 'auto 0',
-            marginRight: '40px',
-            }}> 
-            {calendar[index]?.date}
+        <Box 
+          variant="h4" 
+          component="div" 
+          sx={{ 
+            flexGrow: 1, 
+            flexShrink: 1, 
+            display: 'flex', 
+            justifyContent: 'center' 
+          }}
+        >
+          <Typography 
+            variant="h6" 
+            component="div" 
+            sx={{ 
+              margin: 'auto 0', 
+              marginRight: '40px' 
+            }}
+          >
+            Calendário de agendamento
           </Typography>
         </Box>
-
       </AppBar>
-
     </Box>
   );
 }
